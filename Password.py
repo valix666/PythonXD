68297f6ea3e062fc5300e68c65c439ae205178db
valix666 <valentinap.villamil@gmail.com>
2024-08-12 10:48:37 -0500

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,13 @@
+print ("Ingresa tu contraseña:")
+contraseña= str(input())
+
+if contraseña == "Cu$hufli2022":
+  print ("La contraseña ingresada es correcta")
+else:
+  print ("La contraseña ingresada es incorrecta")
+
+numero= int(input("Ingresa un número entero:"))
+if numero % 2==0:
+  print ("El número ingresado es par")
+else:
+  print ("El número ingresado es impar")
