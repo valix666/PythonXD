e40fbd543d2654d753b4e442c063b4ca61b66732
valix666 <valentinap.villamil@gmail.com>
2024-08-12 10:30:28 -0500

    Create CalculadoraIMC.py

---
+++ CalculadoraIMC.py
@@ -0,0 +1,9 @@
+
+print("Calculo de tu IMC :3")
+print ("Bienvenido")
+print( "Inserta tu peso en kg")
+W= input ()
+print ("Inserta tu altura en metros")
+H= input ()
+IMC= (int(W)/float(H)**2)
+print (f"Tu IMC es de {IMC}")
