27291048f7a0de554fb35efc2b52742036cee2c4
valix666 <valentinap.villamil@gmail.com>
2024-08-12 10:34:15 -0500

    Create Metal-Mecánica.py

---
+++ Metal-Mecánica.py
@@ -0,0 +1,9 @@
+w_engranajes:112 
+w_cilindros:75
+
+engranajes_vendidos=int(input("inserte la cantidad de engranajes vendidos:"))
+
+cilindros_vendidos=int(input("inserte la cantidad de cilindros vendidos"))
+
+total= 112*engranajes_vendidos+ 75*cilindros_vendidos
+print (total, "gr")
